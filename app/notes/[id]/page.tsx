dd0e20d2801aad359f5a747a0551d7010e840ab5
YeVheniia <didenko11121988@ukr.net>
2025-07-14 13:48:05 +0300

    fix: move NoteDetails.client.tsx to [id] and finalize HW-06

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,7 +3,7 @@ import {
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
-import NoteDetailsClient from "../NoteDetails.client";
+import NoteDetailsClient from "./NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
